d137e562b438c515834b21034cabc98cd68e94c3
Dale Gamble <dale.gamble@noaa.gov>
2020-06-30 11:21:58 -0700

    updated required fields

--- gwt/gov/noaa/pmel/sdig/client/panels/InvestigatorPanel.java
+++ gwt/gov/noaa/pmel/sdig/client/panels/InvestigatorPanel.java
@@ -5,10 +5,10 @@ import gov.noaa.pmel.sdig.client.OAPMetadataEditor;
 import gov.noaa.pmel.sdig.shared.bean.Person;
 
 import java.util.*;
-
 /**
  * Created by rhs on 2/27/17.
  */
+
 public class InvestigatorPanel extends PersonPanel {
     public InvestigatorPanel() {
         super("investigator");
@@ -16,6 +16,9 @@ public class InvestigatorPanel extends PersonPanel {
         heading.setText("Enter the Information for this Investigator. You may enter more than one investigator.");
         showTable=true;
         save.setEnabled(false);
+        email.setAllowBlank(true); //allowBlank="false"
+        emailLabel.setText("Email Address");
+        emailLabel.setColor("#000000");
     }
     public void setEditing(boolean isEditing) {
         editing = isEditing;
