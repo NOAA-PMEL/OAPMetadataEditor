39b95ad41441e90319de06ffdfc5aa96db7f8d29
linus.kamb <Linus.Kamb@noaa.gov>
2023-10-09 11:37:32 -0700

    getting syncd up with multiple_gasses

--- grails-app/domain/oap/StandardGas.groovy
+++ grails-app/domain/oap/StandardGas.groovy
@@ -3,25 +3,32 @@ package oap
 class StandardGas {
     static belongsTo = [variable: GenericVariable]
 
+    String gasId
     String manufacturer
     String concentration
     String uncertainty
     String wmoTraceability
 
     static constraints = {
-        manufacturer (nullable: false)
-        concentration (nullable: false)
+        gasId (nullable: true)
+        manufacturer (nullable: true)
+        concentration (nullable: true)
         uncertainty (nullable: true)
         wmoTraceability (nullable: true)
     }
 
-    public StandardGas() {
+    StandardGas() {
+        String gasId = ""
         String manufacturer = ""
         String concentration = ""
         String uncertainty = ""
         String wmoTraceability = ""
     }
     public StandardGas(String manufacturer, String concentration, String uncertainty, String wmoTraceability) {
+        this("", manufacturer, concentration, uncertainty, wmoTraceability);
+    }
+    public StandardGas(String gasId, String manufacturer, String concentration, String uncertainty, String wmoTraceability) {
+        this.gasId = gasId != null ? gasId.trim() : ""
         this.manufacturer = manufacturer != null ? manufacturer.trim() : ""
         this.concentration = concentration != null ? concentration.trim() : ""
         this.uncertainty = uncertainty != null ? uncertainty.trim() : ""
